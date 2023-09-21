eb4d727f0e6d47e9aa766520590bbbf56d155869
frankmollard <116736747+frankmollard@users.noreply.github.com>
2023-09-21 21:36:57 +0200

    Update Stroke_Proba.py

--- Stroke_Proba.py
+++ Stroke_Proba.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingRegressor as GBR
+#from sklearn.ensemble import GradientBoostingRegressor as GBR
 from catboost import CatBoostClassifier
 
 import joblib
