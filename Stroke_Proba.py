dbde548cdcfadfd38446d99f248f7fc934cddc6a
frankmollard <116736747+frankmollard@users.noreply.github.com>
2023-09-22 17:46:34 +0200

    Update Stroke_Proba.py

--- Stroke_Proba.py
+++ Stroke_Proba.py
@@ -334,7 +334,7 @@ tab1.metric(
 )
 
 tab1.text("Confidence in the risk assessment:\n" + \
-          str(((1-uncertainty)*100, 1)) + " %."
+          str((1-uncertainty)*100) + " %."
 )
 
 #######Additional Information##################
