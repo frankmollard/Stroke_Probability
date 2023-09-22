f342f34eb0266a70cd085e873498ec34a48e3c75
frankmollard <116736747+frankmollard@users.noreply.github.com>
2023-09-22 09:59:42 +0200

    Update Stroke_Proba.py

--- Stroke_Proba.py
+++ Stroke_Proba.py
@@ -305,15 +305,13 @@ def delta(l, p):
 #        d=n-o
 #        return d
 
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-else:
-    st.session_state.old = pred - st.session_state.old
 
 if 'old' not in st.session_state:
     st.session_state.old = 0
+    old = pred
 else:
-    st.session_state.old = pred - st.session_state.old
+    old = st.session_state.old
+    st.session_state.old = pred
 
 #Adjustment#########################
 
@@ -329,7 +327,7 @@ tab1.metric(
     label="Risk of Stroke", 
     value=str(round(pred*100/adjst, 1)) + " %", 
     #delta=str(round(delta(userData(), pred)/adjst, 2)) + " percentage points", 
-    delta=str(round(pred/adjst, 2)) + " percentage points", 
+    delta=str(round((pred-old)/adjst, 2)) + " percentage points", 
     help="""
     This is the indication for the risk of stroke, given the patient data.
     The change in percentage points compared to your previous indication is displayed smaller below.
